698291c0e6b715fbcd25062830a54b53de4dfc9d
ALUNO <aluno@hc5mac08.local>
2016-08-10 20:21:54 -0300

    CRIANDO PROJETO INICIAL

---
+++ IESBPOS KNOOWL/IESBPOS KNOOWL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IESBPOS KNOOWL
+//
+//  Created by ALUNO on 10/08/16.
+//  Copyright © 2016 RICARDO BERNARDES. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IESBPOS KNOOWL/IESBPOS KNOOWL/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  IESBPOS KNOOWL
+//
+//  Created by ALUNO on 10/08/16.
+//  Copyright © 2016 RICARDO BERNARDES. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
